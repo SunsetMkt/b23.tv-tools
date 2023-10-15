2ac584a1291be9180efe6733b47f1f3d6a337835
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2023-10-15 14:40:03 +0800

    更多随机信息

--- b23tv.py
+++ b23tv.py
@@ -21,15 +21,15 @@ def get_b23of(long_url):
             + "infoc"
         )
 
-    api = "http://api.bilibili.com/x/share/click"
+    api = "https://api.bilibili.com/x/share/click"
     data = {
-        "build": "6500300",
+        "build": str(random.randint(6000000, 9000000)),
         "buvid": random_buvid(),
         "oid": long_url,
-        "platform": "android",
+        "platform": random.choice(["android", "ios"]),
         "share_channel": "COPY",
         "share_id": "public.webview.0.0.pv",
-        "share_mode": "3",
+        "share_mode": str(random.randint(1, 10)),
     }
     res = requests.post(api, data=data)
     res.raise_for_status()
@@ -40,8 +40,21 @@ def get_b23of(long_url):
         return "Not available"
 
 
-def access_b23_url_and_return_real_url(url: str):
+def access_b23_url_and_return_real_url(url):
     res = requests.get(url, allow_redirects=True)
     real_url = res.url
     r = urlparse(real_url)
     return r.scheme + "://" + r.netloc + r.path
+
+
+def generate_short_url_from_short_url(short_url):
+    long_url = access_b23_url_and_return_real_url(short_url)
+    return get_b23of(long_url)
+
+
+if __name__ == "__main__":
+    test_url = "https://www.bilibili.com/"
+    short = get_b23of(test_url)
+    print(short)
+    long = access_b23_url_and_return_real_url(short)
+    print(long)
