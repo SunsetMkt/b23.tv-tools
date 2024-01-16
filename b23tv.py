b515c7ffe7cfa81e9b9332b2e5002de06c6bcdc6
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-01-16 10:46:05 +0800

    Send fake useragent

--- b23tv.py
+++ b23tv.py
@@ -4,9 +4,12 @@
 import json
 import random
 import string
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse, parse_qs
 
 import requests
+from fake_useragent import UserAgent
+
+ua = UserAgent()
 
 
 def get_b23of(long_url):
@@ -31,27 +34,32 @@ def get_b23of(long_url):
         "share_id": "public.webview.0.0.pv",
         "share_mode": str(random.randint(1, 10)),
     }
+    headers = {
+        "user-agent": ua.random,
+    }
 
     try:
-        res = requests.post(api, data=data)
+        res = requests.post(api, data=data, headers=headers)
         res.raise_for_status()
         data = json.loads(res.content)
         if data["data"].__contains__("content"):
             return data["data"]["content"]
         else:
             return "Not available"
-    except:
-        return "Not available"
+    except Exception as e:
+        return e
 
 
 def access_b23_url_and_return_real_url(url):
     try:
         res = requests.head(url, allow_redirects=True)
         real_url = res.url
-        r = urlparse(real_url)
-        return r.scheme + "://" + r.netloc + r.path
-    except:
-        return "Not available"
+        parsed_url = urlparse(real_url)
+        return urlunparse(
+            (parsed_url.scheme, parsed_url.netloc, parsed_url.path, "", "", "")
+        )
+    except Exception as e:
+        return e
 
 
 def generate_short_url_from_short_url(short_url):
